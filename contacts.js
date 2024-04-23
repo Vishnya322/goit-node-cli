26dd5a48e6c5654eadc7a4f9c3223b7aeeb3f7df
Iryna Vishnevska <Vishnya322@gmail.com>
2024-04-23 21:44:59 +0300

    cli-hw1

---
+++ contacts.js
@@ -0,0 +1,56 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((c) => c.id === contactId);
+    return contact || null;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+      return null;
+    } else {
+      const deletedData = contacts.splice(index, 1)[0];
+      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+      return deletedData;
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = { name, email, phone, id: crypto.randomUUID() };
+    contacts.push(newContact);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+    return newContact;
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
